d85679091ef5bf94537bea2cf240a0280fa7cf54
Priyansh Kedia <kediapriyansh@gmail.com>
2020-03-16 13:38:03 +0530

    Chnaged the menu and cart

--- app/src/main/java/com/example/looters/SplashScreen.java
+++ app/src/main/java/com/example/looters/SplashScreen.java
@@ -1,5 +1,7 @@
 package com.example.looters;
 
+import androidx.annotation.AnimatorRes;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
@@ -7,16 +9,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.animation.Animation;
 import android.widget.TextView;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import javax.xml.transform.Templates;
 
 public class SplashScreen extends AppCompatActivity {
     ConstraintLayout constraintLayout;
     TextView textView;
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,7 +45,27 @@ public class SplashScreen extends AppCompatActivity {
 //            fragmentTransaction = fragmentManager1.beginTransaction();
 //            fragmentTransaction.add(R.id.menuadd,menuFrag);
 //            fragmentTransaction.commit();
+                    int c = account.getEmail().toString().indexOf("@");
+                   String b = account.getEmail().toString().substring(0,c);
+                    DatabaseReference databaseReference1 = FirebaseDatabase.getInstance().getReference().child("menu").child("items");
+                    databaseReference1.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            int i =0;
+                            for(DataSnapshot dataSnapshot1 : dataSnapshot.getChildren())
+                            {
+                                databaseReference.child(b).child(String.valueOf(i)).setValue(dataSnapshot1.getValue());
+                                i++;
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
                     startActivity(new Intent(SplashScreen.this,MenuActivity.class));
+
                 }
                 else {
                     startActivity(new Intent(SplashScreen.this,LoginActivity.class));
